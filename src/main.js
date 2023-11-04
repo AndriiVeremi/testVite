ff442a797c8a7e5bf40ae863acb9315a8b0b658d
Andrii Veremii <dashuk10@gmail.com>
2023-11-04 14:55:15 +0100

    add

--- src/main.js
+++ src/main.js
@@ -2,20 +2,35 @@ import productList from './items.json'
 console.log(productList)
 
 const list = document.querySelector('.js-list');
+list.addEventListener('click', addProduckt)
 
 function markapProductList(arr) {
-    arr.map(
-      ({ id, img, name, price, description }) => `<li class="product-card">
-<img class="product-img" src="" alt="">
-<h2 class="product-title"></h2>
-<h3 class="product-description"></h3>
-<p class="product-price"></p>
+   return arr
+     .map(
+       ({ id, img, name, price, description }) => `<li class="product-card js-item" data-prod-id="${id}">
+<img class="product-img" src="${img}" alt="${name}">
+<h2 class="product-title">${name}</h2>
+<p3 class="product-description">${description}</p3>
+<p class="product-price">${price}</p>
 <button class="product-add-btn">Додати до кошика</button>
 </li>`
-    ).join('')
+     )
+     .join('');
 }
 
-list.insertAdjacentElement('afterbegin', markapProductList(productList))
+list.insertAdjacentHTML('afterbegin', markapProductList(productList));
 
 
-markapProductList(productList);
+function addProduckt(e) {
+
+    if (!e.target.classList.contains('product-add-btn')) {
+         return
+    }
+
+    const productItem = e.target.closest(".js-item");
+    const productId = +productItem.dataset.prodId;
+
+    
+  
+
+}
