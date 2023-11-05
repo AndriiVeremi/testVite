d6423b67799fa624468ce2b8b8528b8362bf4d25
Andrii Veremii <dashuk10@gmail.com>
2023-11-05 10:29:04 +0100

    add

--- src/main.js
+++ src/main.js
@@ -1,40 +1,45 @@
-import productList from './items.json'
-console.log(productList)
+import productList from './items.json';
+console.log(productList);
 
 const list = document.querySelector('.js-list');
-list.addEventListener('click', addProduct)
+list.addEventListener('click', addProduct);
 
-const LS_KEY = "Produckt List Item"
+const LS_KEY = 'Produckt List Item';
 
 function markapProductList(arr) {
-   return arr
-     .map(
-       ({ id, img, name, price, description }) => `<li class="product-card js-item" data-prod-id="${id}">
+  return arr
+    .map(
+      ({
+        id,
+        img,
+        name,
+        price,
+        description,
+      }) => `<li class="product-card js-item" data-prod-id="${id}">
 <img class="product-img" src="${img}" alt="${name}">
 <h2 class="product-title">${name}</h2>
 <p3 class="product-description">${description}</p3>
 <p class="product-price">${price}</p>
 <button class="product-add-btn">Додати до кошика</button>
 </li>`
-     )
-     .join('');
+    )
+    .join('');
 }
 
 list.insertAdjacentHTML('afterbegin', markapProductList(productList));
 
-const bags = []
+let bags = [];
 
 function addProduct(e) {
+  if (!e.target.classList.contains('product-add-btn')) {
+    return;
+  }
 
-    if (!e.target.classList.contains('product-add-btn')) {
-         return
-    }
+  const productItem = e.target.closest('.js-item');
+  const productId = +productItem.dataset.prodId;
+  const product = productList.find(({ id }) => id === productId);
 
-    const productItem = e.target.closest(".js-item");
-    const productId = +productItem.dataset.prodId;
-    const product = productList.find(({ id }) => id === productId);
-
-    bags.push(product)
-}
+  bags.push(product);
 console.log(bags)
-// localStorage.setItem('LS_KEY', JSON.stringify(product));
+  localStorage.setItem('LS_KEY', JSON.stringify(bags));
+}
