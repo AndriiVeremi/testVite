67d324d6c3a4c1208b5278b21ce74cd7dad2dea2
Andrii Veremii <dashuk10@gmail.com>
2023-11-07 12:37:13 +0100

    add

--- src/main.js
+++ src/main.js
@@ -21,3 +21,20 @@ const namesOfMonth = [
   'Листопад',
   'Грудень',
 ];
+
+const refs = {
+  day: document.querySelector('.date-day'),
+  date: document.querySelector('.date'),
+  month: document.querySelector('.date-month'),
+  year: document.querySelector('.date-year'),
+  clock: document.querySelector('.digital-clock'),
+};
+
+setInterval(() => {
+    const currentDate = new Date()
+    refs.day.textContent = arrDays[currentDate.getDay()];
+    refs.date.textContent = currentDate.getDate();
+    refs.month.textContent = namesOfMonth[currentDate.getMonth()];
+    refs.year.textContent = currentDate.getFullYear();
+    refs.clock.textContent = currentDate.toLocaleTimeString('uk');
+}, 1000)
